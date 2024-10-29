a5ad221350aec70084e00ed442001d30a8bb4c8d
hu13 <hangkun.ung@gmail.com>
2024-10-29 15:50:08 -0400

    Init

--- Makefile
+++ Makefile
@@ -25,13 +25,17 @@ clean: $(clean_targets)
 
 .PHONY: gen-files
 gen-files:
-	rm -rf $(CURDIR)/windows
+	# rm -rf $(CURDIR)/windows
 	go generate github.com/saltosystems/winrt-go/...
+
+.PHONY: pv
+pv:
+	go generate github.com/saltosystems/winrt-go/winrt_preveil.go
 
 .PHONY: check-generated
 check-generated: export WINRT_GO_GEN_VALIDATE=1
 check-generated:
-	 go generate github.com/saltosystems/winrt-go/...
+	go generate github.com/saltosystems/winrt-go/...
 
 .PHONY: go-test
 go-test:
---
+++ winrt_preveil.go
@@ -0,0 +1,7 @@
+package winrt
+
+// for preveil cloud drive
+//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Storage.Provider.StorageProviderItemProperty
+//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Foundation.Uri
+//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Foundation.IStringable
+//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Foundation.WwwFormUrlDecoder
